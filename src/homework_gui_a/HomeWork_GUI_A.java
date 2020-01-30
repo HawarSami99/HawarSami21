6f457feeb94fa4792883f9cf7569a50a217b6e9e
Hawar Sami <hawar.313185019@uhd.edu.iq>
2020-01-30 23:41:36 +0300

    Edited

--- src/homework_gui_a/HomeWork_GUI_A.java
+++ src/homework_gui_a/HomeWork_GUI_A.java
@@ -6,31 +6,32 @@ import javax.swing.JOptionPane;
 
 
 public class HomeWork_GUI_A {
-   public void checkEO(){
+   public void ShowData(){
      
  HomeWork_GUI_A g=new HomeWork_GUI_A();
- g.checkEO();
-        String input=JOptionPane.showInputDialog("please input a number");
+ g.ShowData();
+        String input=JOptionPane.showInputDialog("please input a number"+JOptionPane.QUESTION_MESSAGE);
 int aa=Integer.parseInt(input);
 if (aa%2==0){
 JOptionPane.showMessageDialog(null,"The number is Even"+JOptionPane.INFORMATION_MESSAGE);
 } else{
 JOptionPane.showMessageDialog(null,"The number is Odd"+JOptionPane.INFORMATION_MESSAGE);
-
-
-    JFrame frame = new JFrame();
-    String message = "Try another Number?";
-    int answer = JOptionPane.showConfirmDialog(frame, message);
-    while (answer == JOptionPane.YES_OPTION) {
-g.checkEO();
-
-
 }
-       while (answer == JOptionPane.NO_OPTION) {
+   JFrame f=new JFrame();  
+ 
+ int a=JOptionPane.showConfirmDialog(f,"Try Again?");  
+if(a==JOptionPane.YES_OPTION)
+
+g.ShowData();
+
+
+else{
+    JOptionPane.showMessageDialog(f,JOptionPane.NO_OPTION);
+      
     System.exit(0);       
        }
    }
 }
-}
+
     
 
---
+++ src/homework_gui_a/TstGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework_gui_a;
+
+
+
+
+
+/**
+ *
+ * @author Hawar
+ */
+public class TstGUI {
+    public static void main(String[] args) {
+        HomeWork_GUI_A g=new HomeWork_GUI_A();
+       g.ShowData();
+    }
+}
